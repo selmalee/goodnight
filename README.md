8d041a9285ab96a3d357878c54dcf78294eaf631
seminelee <747191197@qq.com>
2019-06-04 01:06:21 +0800

    [-]. react navigation

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
-### 编译并运行
+## 编译并运行
 ``` bash
 react-native run-ios
 ```
 
-### 报错记录
-**Could not find iPhone 6 simulator**
+## 报错记录
+#### Could not find iPhone 6 simulator
 node_modules/react-native/local-cli/runIOS/findMatchingSimulator.js line30
 Replace ```version.indexOf('iOS') !== 0``` with ``` !version.includes('iOS') ```
 
-**GoodNight has not been registered**
+#### GoodNight has not been registered
 进程冲突，关闭模拟器后重新运行
 或者是index.ios.js与./ios/项目名/appDelegate.m或者./android/app/src/main/java/com/项目名/MainActivity.java中的项目名不一致
 
-**no bundle url present**
+#### no bundle url present
  1. 
  ``` bash
  cd ios
@@ -25,6 +25,3 @@ ios/GoodNight/AppDelegate.m
   // jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
   jsCodeLocation = [NSURL URLWithString:@"http://127.0.0.1:8081/index.ios.bundle?platform=ios&dev=true"]; 
 ```
-
-**Print: Entry, ":CFBundleIdentifier", Does Not Exist**
-
--- pages/App.js
+++ pages/App.js
@@ -1,35 +1,34 @@
 import React, { Component } from 'react';
-// import { AsyncStorage } from "react-native"
-// import Welcome from './Welcome';
-// import Goodnight from './Home';
-// import {
-//   View,
-//   Text
-// } from 'react-native'
+import { AsyncStorage } from "react-native"
+import Welcome from './Welcome';
+import Home from './Home';
+import {
+  View,
+  Text
+} from 'react-native'
 import styles from '../styles/app.style'
-import AppContainer from './AppContainer';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-    // this.state = {
-    //   loading: true,
-    //   username: ''
-    // }
-    // this.getUsername()
+    this.state = {
+      loading: true,
+      username: ''
+    }
+    this.getUsername()
   }
 
-  // async getUsername() {
-  //   const value = await AsyncStorage.getItem('username');
-  //   this.setState({
-  //     username: value,
-  //     loading: false
-  //   })
-  // }
+  async getUsername() {
+    const value = await AsyncStorage.getItem('username');
+    this.setState({
+      username: value,
+      loading: false
+    })
+  }
 
   render() {
     return (
-      <AppContainer style={styles.container} />
-    );
+      <Home />
+    )
   }
 }
